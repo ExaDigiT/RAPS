2d60432c9da6e950a98085226178b7f1da4288d2
Matthias Maiterth <maiterthm@ornl.gov>
2025-03-07 15:26:37 -0500

    Update of Marconi for explict parameters of job_dict only

--- raps/dataloaders/marconi100.py
+++ raps/dataloaders/marconi100.py
@@ -207,9 +207,12 @@ def load_data_from_df(jobs_df: pd.DataFrame, **kwargs):
 
         if gpu_trace.size > 0 and (jid == job_id or jid == '*'):  # and time_submit >= 0:
 
-            job_info = job_dict(nodes_required, name, account, cpu_trace, gpu_trace, [], [],
-                                end_state, scheduled_nodes,
-                                job_id, priority, partition,
+            job_info = job_dict(nodes_required=nodes_required, name=name,
+                                account=account, cpu_trace=cpu_trace,
+                                gpu_trace=gpu_trace, nrx_trace=[],ntx_trace=[],
+                                end_state=end_state,
+                                scheduled_nodes=scheduled_nodes,
+                                id=job_id, priority=priority,partition=partition,
                                 submit_time=submit_time, time_limit=time_limit,
                                 start_time=start_time, end_time=end_time,
                                 wall_time=wall_time, trace_time=trace_time,
